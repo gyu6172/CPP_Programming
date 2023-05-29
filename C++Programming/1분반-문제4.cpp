128bb5a8ce228d4f6b0401659a99d5167370b0a2
gyu6172 <gyu6172@gmail.com>
2023-05-29 17:30:14 +0900

    0529-1분반 문제4 +=, + 연산자 제외 모두 구현

--- C++Programming/1분반-문제4.cpp
+++ C++Programming/1분반-문제4.cpp
@@ -1,99 +1,65 @@
 #include <iostream>
-#include <string>
+using namespace std;
+
 int getLength(const char* str) {
 	int len=0;
-	for (int i = 0; str[i] != NULL; i++) {
+	while (true) {
+		if(str[len]==NULL)
+			break;
 		len++;
 	}
 	return len;
 }
-using namespace std;
+
 class MyString {
 private:
 	char* m_str;
+	int m_length;
 
 public:
-	MyString(){
-		
-	}
-	MyString(const char* str){
-		m_str = new char[getLength(str)+1];
-		for (int i = 0; str[i] != NULL; i++) {
+	MyString(const char* str)
+	{
+		m_length = getLength(str);
+		m_str = new char[m_length+1];
+		for (int i = 0; i < m_length; i++) {
 			m_str[i] = str[i];
 		}
-		m_str[getLength(str)] = NULL;
+		m_str[m_length] = NULL;
 	}
-	MyString(MyString& in) {
-		m_str = new char[getLength(in.m_str)+1];
-		for (int i = 0; i < getLength(in.m_str); i++) {
-			m_str[i] = (in.m_str)[i];
+
+	MyString(const MyString& in) {
+		m_length = in.m_length;
+		m_str = new char[m_length+1];
+		for (int i = 0; i < m_length; i++) {
+			m_str[i] = in.m_str[i];
 		}
-		m_str[getLength(in.m_str)] = NULL;
-
+		m_str[m_length] = NULL;
 	}
-	void Set(const char* str);
-	void print();
 
-	MyString& operator+(const char* str);
-	friend MyString& operator+(const char* str, MyString& in);
-	MyString& operator+=(const char* str);
+	void Set(const char* in) {
+		delete m_str;
+		m_length = getLength(in);
+		m_str = new char[m_length + 1];
+		for (int i = 0; i < m_length; i++) {
+			m_str[i] = in[i];
+		}
+		m_str[m_length] = NULL;
+	}
+
+	friend ostream& operator<<(ostream& o, MyString& in);
 };
+
+ostream& operator<<(ostream& o, MyString& in) {
+	o<<in.m_str;
+	return o;
+}
+
 int main() {
 	MyString a("Sejong");
 	MyString b = a;
 
 	a.Set("University");
 
-	a.print();
-	b.print();
-
-	return 0;
-}
-
-void MyString::Set(const char* str)
-{
-	delete m_str;
-	m_str = new char[getLength(str)+1];
-	for (int i = 0; str[i] != NULL; i++) {
-		m_str[i] = str[i];
-	}
-	m_str[getLength(str)] = NULL;
-}
-
-void MyString::print() {
-	cout<<m_str<<endl;
-}
-
-MyString& MyString::operator+(const char* str) {
-	int len = getLength(str) + getLength(m_str) + 1;
-	char* out = new char[len];
-	int i=0;
-	for (; i < len-1; i++) {
-		if (i < getLength(m_str)) {
-			out[i] = m_str[i];
-		}
-		else {
-			out[i] = str[i-getLength(m_str)];
-		}
-	}
-	out[i] = NULL;
-}
-
-MyString& operator+(const char* str, MyString& in) {
-	int len = getLength(str) + getLength(in.m_str) + 1;
-	char* out = new char[len];
-	int i = 0;
-	for (; i < len - 1; i++) {
-		if (i < getLength(str)) {
-			out[i] = str[i];
-		}
-		else {
-			out[i] = in.m_str[i-getLength(str)];
-		}
-	}
-	out[i] = NULL;
-}
-
-MyString& MyString::operator+=(const char* str) {
-	
+	cout<<a<<endl;
+	cout<<b<<endl;
 }
